2d09476c745f365208a7b2549a58b98f3dcd3195
faiss <faissel50@hotmail.com>
2023-10-21 19:02:22 +0200

    wachtwoord aanpassen test1

--- app/src/main/java/com/example/hirehub/landingtestFragment.kt
+++ app/src/main/java/com/example/hirehub/landingtestFragment.kt
@@ -1,11 +1,13 @@
 package com.example.hirehub
 
+import AccountActivity
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.hirehub.LoginActivity
 import com.example.hirehub.Profileboard.ProfileboardActivity
 import com.example.hirehub.UserProfile.UserProfileActivity
 import com.example.hirehub.Userboard.UserboardActivity
@@ -40,14 +42,13 @@ class landingtestFragment : Fragment() {
             // Starten van ProfileboardActivity wanneer de button wordt geklikt
             val intent = Intent(requireActivity(), ProfileboardActivity::class.java)
             startActivity(intent)
-
         }
+
         // Koppelen van klikactie aan de RegistratieButton
         binding.registrationButton.setOnClickListener {
-            // Starten van RegistationActivity wanneer de button wordt geklikt
+            // Starten van RegistrationActivity wanneer de button wordt geklikt
             val intent = Intent(requireActivity(), RegistrationActivity::class.java)
             startActivity(intent)
-
         }
 
         // Koppelen van klikactie aan de loginButton
@@ -57,10 +58,17 @@ class landingtestFragment : Fragment() {
             startActivity(intent)
         }
 
-        // Koppelen van klikactie aan de loginButton
+        // Koppelen van klikactie aan de userProfileButton
         binding.userProfileButton.setOnClickListener {
-            // Starten van LoginActivity wanneer de button wordt geklikt
+            // Starten van UserProfileActivity wanneer de button wordt geklikt
             val intent = Intent(requireActivity(), UserProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Koppelen van klikactie aan de accountButton
+        binding.btnMyAccount.setOnClickListener {
+            // Starten van AccountActivity wanneer de knop wordt geklikt
+            val intent = Intent(requireActivity(), AccountActivity::class.java)
             startActivity(intent)
         }
 
@@ -70,6 +78,16 @@ class landingtestFragment : Fragment() {
 
         // Update de welkomsttekst met de gebruikersnaam
         binding.welcomeUsername.text = "Hello, $username"
+
+        // Controleer of de gebruiker is ingelogd
+        val isUserLoggedIn = sessionManager.getUserId() != -1
+
+        // Pas de zichtbaarheid van de accountButton aan op basis van de inlogstatus
+        if (isUserLoggedIn) {
+            binding.btnMyAccount.visibility = View.VISIBLE
+        } else {
+            binding.btnMyAccount.visibility = View.GONE
+        }
     }
 
     override fun onDestroyView() {
@@ -78,3 +96,99 @@ class landingtestFragment : Fragment() {
         _binding = null
     }
 }
+//import android.content.Intent
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.fragment.app.Fragment
+//import com.example.hirehub.AccountActivity
+//import com.example.hirehub.LoginActivity
+//import com.example.hirehub.Profileboard.ProfileboardActivity
+//import com.example.hirehub.UserProfile.UserProfileActivity
+//import com.example.hirehub.Userboard.UserboardActivity
+//import com.example.hirehub.databinding.FragmentLandingtestBinding
+//import com.example.hirehub.utils.SessionManager
+//
+//class landingtestFragment : Fragment() {
+//
+//    private var _binding: FragmentLandingtestBinding? = null
+//    private val binding get() = _binding!!
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        _binding = FragmentLandingtestBinding.inflate(inflater, container, false)
+//        return binding.root
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        // Koppelen van klikactie aan de userboardButton
+//        binding.userboardButton.setOnClickListener {
+//            // Starten van UserboardActivity wanneer de button wordt geklikt
+//            val intent = Intent(requireActivity(), UserboardActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        // Koppelen van klikactie aan de profileButton
+//        binding.profileboardButton.setOnClickListener {
+//            // Starten van ProfileboardActivity wanneer de button wordt geklikt
+//            val intent = Intent(requireActivity(), ProfileboardActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        // Koppelen van klikactie aan de RegistratieButton
+//        binding.registrationButton.setOnClickListener {
+//            // Starten van RegistrationActivity wanneer de button wordt geklikt
+//            val intent = Intent(requireActivity(), RegistrationActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        // Koppelen van klikactie aan de loginButton
+//        binding.loginButton.setOnClickListener {
+//            // Starten van LoginActivity wanneer de button wordt geklikt
+//            val intent = Intent(requireActivity(), LoginActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        // Koppelen van klikactie aan de userProfileButton
+//        binding.userProfileButton.setOnClickListener {
+//            // Starten van UserProfileActivity wanneer de button wordt geklikt
+//            val intent = Intent(requireActivity(), UserProfileActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        // Koppelen van klikactie aan de accountButton
+//        binding.btnMyAccount.setOnClickListener {
+//            // Starten van AccountActivity wanneer de knop wordt geklikt
+//            val intent = Intent(requireActivity(), AccountActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        // Haal de gebruikersnaam op uit de SessionManager
+//        val sessionManager = SessionManager(requireContext())
+//        val username = sessionManager.getUsername()
+//
+//        // Update de welkomsttekst met de gebruikersnaam
+//        binding.welcomeUsername.text = "Hello, $username"
+//
+//        // Controleer of de gebruiker is ingelogd
+//        val isUserLoggedIn = sessionManager.getUserId() != -1
+//
+//        // Pas de zichtbaarheid van de accountButton aan op basis van de inlogstatus
+//        if (isUserLoggedIn) {
+//            binding.btnMyAccount.visibility = View.VISIBLE
+//        } else {
+//            binding.btnMyAccount.visibility = View.GONE
+//        }
+//    }
+//
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        // Opruimen van de binding om geheugenlekken te voorkomen
+//        _binding = null
+//    }
+//}
