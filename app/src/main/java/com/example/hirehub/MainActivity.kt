da3bee8b8c898e48630f8bb3b61664e2efc61bd6
Gallzor <s1181531@student.windesheim.nl>
2023-10-22 16:50:40 +0200

    fixed the menu buttons in mainactivity so they will respond to their activities.

--- app/src/main/java/com/example/hirehub/MainActivity.kt
+++ app/src/main/java/com/example/hirehub/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.hirehub
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
+import com.example.hirehub.Profileboard.ProfileboardActivity
+import com.example.hirehub.UserProfile.UserProfileActivity
+import com.example.hirehub.Userboard.UserboardActivity
 import com.example.hirehub.databinding.ActivityMainBinding
 import com.example.hirehub.utils.SessionManager
 
@@ -22,10 +27,14 @@ class MainActivity : AppCompatActivity() {
         // Initialiseer de SessionManager
         sessionManager = SessionManager(this)
 
+        // Haal de gebruikersrol en het gebruikers-ID op
+        val userRole = sessionManager.getUserRole()
+        val userId = sessionManager.getUserId()
+
         // Zoek de accountButton en voeg een kliklistener toe
         val accountButton = findViewById<View>(R.id.accountButton)
 
-        if (sessionManager.getUserId() == -1) {
+        if (userId == -1) {
             // Er is geen gebruiker ingelogd, stuur door naar LoginActivity
             accountButton.setOnClickListener {
                 val intent = Intent(this, LoginActivity::class.java)
@@ -36,6 +45,45 @@ class MainActivity : AppCompatActivity() {
             accountButton.setOnClickListener {
                 val intent = Intent(this, AccountActivity::class.java)
                 startActivity(intent)
+            }
+
+            // Zoek de knoppen
+            val homepageButton = findViewById<ImageButton>(R.id.homepageButton)
+            val userCrudButton = findViewById<ImageButton>(R.id.userCrudButton)
+            val profileCrudButton = findViewById<ImageButton>(R.id.profileCrudButton)
+
+            // Logica voor knoppen op basis van gebruikersrol
+            when (userRole?.toLowerCase()) {
+                "sol", "sollicitant" -> {
+                    // Gebruiker is een sollicitant
+                    userCrudButton.visibility = View.GONE
+                    profileCrudButton.setOnClickListener {
+                        val intent = Intent(this, UserProfileActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+                "rec", "recruiter" -> {
+                    // Gebruiker is een recruiter
+                    userCrudButton.visibility = View.GONE
+                    profileCrudButton.setOnClickListener {
+                        val intent = Intent(this, ProfileboardActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+                "ad", "admin" -> {
+                    // Gebruiker is een admin
+                    // Toon de userCrudButton en stuur profileCrudButton naar ProfileboardActivity
+                    userCrudButton.visibility = View.VISIBLE
+                    profileCrudButton.setOnClickListener {
+                        val intent = Intent(this, ProfileboardActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+                else -> {
+                    // Gebruiker heeft een onbekende rol (toon geen knoppen)
+                    userCrudButton.visibility = View.GONE
+                    profileCrudButton.visibility = View.GONE
+                }
             }
         }
     }
