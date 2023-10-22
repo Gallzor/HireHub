de828ba5cfd3be08ced4efe2c857f4966f113703
Gallzor <s1181531@student.windesheim.nl>
2023-10-22 16:30:19 +0200

    fixed login and registration into fragments so buttons are properly set

--- app/src/main/java/com/example/hirehub/RegistrationActivity.kt
+++ app/src/main/java/com/example/hirehub/RegistrationActivity.kt
@@ -10,66 +10,24 @@ import com.example.hirehub.databases.HireHubApplication
 import com.example.hirehub.databinding.ActivityLoginBinding
 import com.example.hirehub.databinding.ActivityRegistrationBinding
 import com.example.hirehub.models.User
+import com.example.hirehub.utils.SessionManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class RegistrationActivity : AppCompatActivity() {
 
-    lateinit var etUsername: EditText
-    lateinit var etPassword: EditText
-    lateinit var btnRegister: Button
     private lateinit var binding: ActivityRegistrationBinding
+    private lateinit var sessionManager: SessionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
-
-        // Initialisatie van de binding
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        etUsername = findViewById(R.id.etUsername)
-        etPassword = findViewById(R.id.etPassword)
-        btnRegister = findViewById(R.id.btnRegister)
+        // Initialiseer de SessionManager
+        sessionManager = SessionManager(this)
 
-        btnRegister.setOnClickListener {
-            val username = etUsername.text.toString()
-            val password = etPassword.text.toString()
-
-            if (validateInputs(username, password)) {
-                CoroutineScope(Dispatchers.IO).launch {
-                    val newUser = User(username, password)
-                    saveUser(newUser)
-
-                    runOnUiThread {
-                        Toast.makeText(this@RegistrationActivity, "Registratie succesvol!", Toast.LENGTH_SHORT).show()
-                        navigateToLogin()
-                    }
-                }
-            } else {
-                Toast.makeText(this, "Vul alle velden correct in.", Toast.LENGTH_SHORT).show()
-            }
-            // Koppelen van klikactie aan de loginBackButton
-            binding.registrationBackButton.setOnClickListener {
-                finish() // Dit zal teruggaan naar het vorige scherm
-            }
-
-        }
-    }
-
-    private fun validateInputs(username: String, password: String): Boolean {
-        return username.isNotEmpty() && password.isNotEmpty()
-    }
-
-    private suspend fun saveUser(user: User) {
-        val app = application as? HireHubApplication ?: return
-        val userRepository = app.userRepository
-        userRepository.insertUser(user)
-    }
-
-    private fun navigateToLogin() {
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-        finish() // Optioneel: sluit de huidige registratieactiviteit
     }
 }
+
