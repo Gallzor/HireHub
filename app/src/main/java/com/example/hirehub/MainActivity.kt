82f7ea5567ae80483f88b27abe4af66780b8a53b
Gallzor <s1181531@student.windesheim.nl>
2023-10-22 17:47:50 +0200

    added seeding data of 3 users to mainactivity. fixed roles. Fixed menu to roles

--- app/src/main/java/com/example/hirehub/MainActivity.kt
+++ app/src/main/java/com/example/hirehub/MainActivity.kt
@@ -1,28 +1,39 @@
 package com.example.hirehub
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
-import com.google.android.material.snackbar.Snackbar
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.ui.AppBarConfiguration
 import com.example.hirehub.Profileboard.ProfileboardActivity
 import com.example.hirehub.UserProfile.UserProfileActivity
 import com.example.hirehub.Userboard.UserboardActivity
+import com.example.hirehub.databases.HireHubApplication
 import com.example.hirehub.databinding.ActivityMainBinding
+import com.example.hirehub.factories.UserModelFactory
 import com.example.hirehub.utils.SessionManager
+import com.example.hirehub.viewmodels.UserViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var sessionManager: SessionManager
 
+    private val userViewModel: UserViewModel by viewModels {
+        UserModelFactory((application as HireHubApplication).userRepository)
+    }
+
+    @SuppressLint("DefaultLocale")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Maak 3 gebruikers aan en seed ze in de database.
+        // Roep de seedDatabase-functie aan wanneer de app wordt gestart
+        userViewModel.seedDatabase()
 
         // Initialiseer de SessionManager
         sessionManager = SessionManager(this)
@@ -40,6 +51,12 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this, LoginActivity::class.java)
                 startActivity(intent)
             }
+
+            // Verberg de knoppen
+            val userCrudButton = findViewById<ImageButton>(R.id.userCrudButton)
+            val profileCrudButton = findViewById<ImageButton>(R.id.profileCrudButton)
+            userCrudButton.visibility = View.GONE
+            profileCrudButton.visibility = View.GONE
         } else {
             // Er is een gebruiker ingelogd, stuur door naar AccountActivity
             accountButton.setOnClickListener {
@@ -54,7 +71,7 @@ class MainActivity : AppCompatActivity() {
 
             // Logica voor knoppen op basis van gebruikersrol
             when (userRole?.toLowerCase()) {
-                "sol", "sollicitant" -> {
+                "sol", "sollicitant", "sol" -> {
                     // Gebruiker is een sollicitant
                     userCrudButton.visibility = View.GONE
                     profileCrudButton.setOnClickListener {
@@ -62,7 +79,8 @@ class MainActivity : AppCompatActivity() {
                         startActivity(intent)
                     }
                 }
-                "rec", "recruiter" -> {
+
+                "rec", "recruiter", "rec" -> {
                     // Gebruiker is een recruiter
                     userCrudButton.visibility = View.GONE
                     profileCrudButton.setOnClickListener {
@@ -70,15 +88,21 @@ class MainActivity : AppCompatActivity() {
                         startActivity(intent)
                     }
                 }
-                "ad", "admin" -> {
+
+                "ad", "admin", "ad" -> {
                     // Gebruiker is een admin
                     // Toon de userCrudButton en stuur profileCrudButton naar ProfileboardActivity
                     userCrudButton.visibility = View.VISIBLE
+                    userCrudButton.setOnClickListener {
+                        val intent = Intent(this, UserboardActivity::class.java)
+                        startActivity(intent)
+                    }
                     profileCrudButton.setOnClickListener {
                         val intent = Intent(this, ProfileboardActivity::class.java)
                         startActivity(intent)
                     }
                 }
+
                 else -> {
                     // Gebruiker heeft een onbekende rol (toon geen knoppen)
                     userCrudButton.visibility = View.GONE
